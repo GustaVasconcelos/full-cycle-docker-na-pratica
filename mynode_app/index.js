e715ca953f423084697375c9006f427220714275
gustavasconcelos <gustavos.vasconcelos.peixoto18@gmail.com>
2024-12-08 10:15:36 -0300

    feat: finaliza construção da imagem node

---
+++ mynode_app/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000
+const message = process.env.MESSAGE || 'Hello World'
+
+app.get("/", (req,res) => {
+    res.send(message)
+})
+
+app.listen(port, () => console.log("Servidor rodando na porta: ", port))
